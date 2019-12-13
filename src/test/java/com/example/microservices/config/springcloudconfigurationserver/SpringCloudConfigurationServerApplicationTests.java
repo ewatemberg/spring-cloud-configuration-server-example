2c0093c18fd7c039d2590008dc8ba29e81487ad1
Emilio Watemberg <emilio.watemberg@synapse.com>
2019-12-13 10:31:45 -0300

    First commit

---
+++ src/test/java/com/example/microservices/config/springcloudconfigurationserver/SpringCloudConfigurationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.microservices.config.springcloudconfigurationserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudConfigurationServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
